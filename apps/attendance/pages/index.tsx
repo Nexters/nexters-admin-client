ce102fccc48dbe6c24ac1d7b1b25beebe9ad172c
Minsu Kim <alstn2468_@naver.com>
2023-01-15 22:32:10 +0900

    feat: admin, attendance 프로젝트 모듈 절대경로 설정 추가

--- apps/attendance/pages/index.tsx
+++ apps/attendance/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@weekly/ui';
+import Hello from '~/components/Hello';
 
 export default function Home() {
   return (
     <div>
       <h1>Attendance</h1>
       <Button />
+      <Hello />
     </div>
   );
 }

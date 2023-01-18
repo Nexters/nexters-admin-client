fe412b7a95630dbba1ce8deb20e51454a6c91f4b
Minsu Kim <alstn2468_@naver.com>
2023-01-18 17:17:57 +0900

    feat: admin, attendance 프로젝트 모듈 절대경로 설정 추가 (#3)
    * feat: admin, attendance 프로젝트 모듈 절대경로 설정 추가
    
    * feat: lint-staged 설정 다시 추가

--- apps/admin/pages/index.tsx
+++ apps/admin/pages/index.tsx
@@ -1,10 +1,14 @@
 import { Button } from '@weekly/ui';
+import React from 'react';
+
+import Hello from '~/components/Hello';
 
 export default function Home() {
   return (
     <div>
       <h1>Admin</h1>
       <Button />
+      <Hello />
     </div>
   );
 }
---
+++ apps/attendance/components/Hello.tsx
@@ -0,0 +1,5 @@
+function Hello() {
+  return <h1>Hello World!</h1>;
+}
+
+export default Hello;

5b07fd172d74f55d1e8cd9af396899823637949e
Minsu Kim <alstn2468_@naver.com>
2023-02-05 23:58:14 +0900

    chore: Link 잘 안보여서 div로감쌈

--- apps/admin/pages/admin/attendance.tsx
+++ apps/admin/pages/admin/attendance.tsx
@@ -4,11 +4,21 @@ function Attendance() {
   return (
     <div>
       <h1>Attendance</h1>
-      <Link href="/admin/attendance">출석관리</Link>
-      <Link href="/admin/activity">활동관리</Link>
-      <Link href="/admin/user">회원관리</Link>
-      <Link href="/admin/authentication/login">로그인</Link>
-      <Link href="/admin/authentication/logout">로그아웃</Link>
+      <div>
+        <Link href="/admin/attendance">출석관리</Link>
+      </div>
+      <div>
+        <Link href="/admin/activity">활동관리</Link>
+      </div>
+      <div>
+        <Link href="/admin/user">회원관리</Link>
+      </div>
+      <div>
+        <Link href="/admin/authentication/login">로그인</Link>
+      </div>
+      <div>
+        <Link href="/admin/authentication/logout">로그아웃</Link>
+      </div>
     </div>
   );
 }

e93c272952d1777714b55ff5f9093ab883213565
한규진 <david0218@naver.com>
2023-02-06 20:29:37 +0900

    feat : Button 컴포넌트

--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -7,10 +7,21 @@ export interface ButtonProps extends React.ComponentProps<'button'> {
 type Props = Partial<ButtonProps>;
 
 function Button(props: Props) {
-  const { children } = props;
-  return <Container>{children}</Container>;
+  const { children, fullWidth } = props;
+  return <Container fullWidth={fullWidth}>{children}</Container>;
 }
 
-const Container = styled.button<Props>``;
+const Container = styled.button<Props>`
+  ${({ theme }) => theme.typo.body1Bold}
+  color: ${({ theme }) => theme.palette.grayScale.g100};
+
+  padding: 16px 24px;
+  border-radius: 16px;
+  width: ${({ fullWidth }) => (fullWidth ? '100%' : 'auto')};
+  background-color: ${({ theme }) => theme.palette.main.green50};
+  &:active {
+    background-color: ${({ theme }) => theme.palette.main.green100};
+  }
+`;
 
 export { Button };

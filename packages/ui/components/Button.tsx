e049d73f03d994ff7f2a3b6d984147115fa1f59d
한규진 <david0218@naver.com>
2023-02-06 20:40:02 +0900

    chore : Button disabled style

--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -8,7 +8,11 @@ type Props = Partial<ButtonProps>;
 
 function Button(props: Props) {
   const { children, fullWidth } = props;
-  return <Container fullWidth={fullWidth}>{children}</Container>;
+  return (
+    <Container fullWidth={fullWidth} {...props}>
+      {children}
+    </Container>
+  );
 }
 
 const Container = styled.button<Props>`
@@ -19,8 +23,14 @@ const Container = styled.button<Props>`
   border-radius: 16px;
   width: ${({ fullWidth }) => (fullWidth ? '100%' : 'auto')};
   background-color: ${({ theme }) => theme.palette.main.green50};
+
   &:active {
     background-color: ${({ theme }) => theme.palette.main.green100};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.palette.grayScale.g40};
+    color: ${({ theme }) => theme.palette.grayScale.g70};
   }
 `;
 

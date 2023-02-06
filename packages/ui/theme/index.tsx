c68e91bc179fa6a204187970fce038e0a1aee109
한규진 <david0218@naver.com>
2023-02-06 19:52:18 +0900

    fix : emotion.d.ts 타입 수정

--- packages/ui/theme/index.tsx
+++ packages/ui/theme/index.tsx
@@ -2,7 +2,9 @@ import { Global, ThemeProvider as BaseThemeProvider } from '@emotion/react';
 import { PropsWithChildren } from 'react';
 
 import { globalStyle } from './global';
+import { palette } from './palette';
 import { media, theme } from './theme';
+import { typo } from './typo';
 
 function ThemeProvider(props: PropsWithChildren<unknown>) {
   const { children } = props;
@@ -14,4 +16,4 @@ function ThemeProvider(props: PropsWithChildren<unknown>) {
   );
 }
 
-export { media, ThemeProvider };
+export { media, palette, ThemeProvider, typo };

fc0a937b505e022e4ff40c3000ce1ec229aa87f9
Minsu Kim <alstn2468_@naver.com>
2023-02-07 23:42:28 +0900

    feat: import 관련 eslint 룰 추가

--- packages/eslint-config-weekly/index.js
+++ packages/eslint-config-weekly/index.js
@@ -1,6 +1,6 @@
 module.exports = {
-  extends: ['next', 'turbo', 'prettier'],
-  plugins: ['eslint-plugin-simple-import-sort', 'unused-imports'],
+  plugins: ['import', 'eslint-plugin-simple-import-sort', 'unused-imports'],
+  extends: ['next', 'turbo', 'prettier', 'plugin:import/recommended'],
   rules: {
     'react/jsx-key': 'off',
     'react/no-unescaped-entities': 'off',
@@ -13,10 +13,10 @@ module.exports = {
     ],
     'react/jsx-first-prop-new-line': 'error',
     'comma-dangle': ['error', 'always-multiline'],
-    indent: ['error', 2],
-    quotes: ['error', 'single'],
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
     'eol-last': 'error',
-    semi: ['error', 'always'],
+    'semi': ['error', 'always'],
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
     'no-unused-vars': [
@@ -25,5 +25,7 @@ module.exports = {
     ],
     'unused-imports/no-unused-imports-ts': ['error'],
     'comma-spacing': ['error', { before: false, after: true }],
+    'import/consistent-type-specifier-style': ['error', 'prefer-top-level'],
+    'import/newline-after-import': ['error', { count: 1, considerComments: true }],
   },
 };

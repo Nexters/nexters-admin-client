d3a95df6d45d37e1f72ee7a55c3c02e15c68f88b
한규진 <55226431+9yujin@users.noreply.github.com>
2023-02-08 01:00:18 +0900

    feat : typo lineheight 추가, Button 컴포넌트 (#28)
    * fix : emotion.d.ts 타입 수정
    
    * chore : theme typo line-height 설정 추가
    
    * feat : Button 컴포넌트
    
    * chore : Button disabled style
    
    * refac: ui 패키지에서 typo 관련 객체 만드는 부분 함수로 리팩토링 (#30)
    
    * refac: typoObject 만드는 로직 추가
    
    * chore: 함수 이름 변경
    
    * refac: export 구문 동일하게 수정
    
    * chore : 리뷰 반영
    
    ---------
    
    Co-authored-by: Minsu Kim <alstn2468_@naver.com>

--- packages/ui/components/Button.tsx
+++ packages/ui/components/Button.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 
+import { rem } from '../theme';
+
 export interface ButtonProps extends React.ComponentProps<'button'> {
   fullWidth: boolean;
 }
@@ -7,10 +9,31 @@ export interface ButtonProps extends React.ComponentProps<'button'> {
 type Props = Partial<ButtonProps>;
 
 function Button(props: Props) {
-  const { children } = props;
-  return <Container>{children}</Container>;
+  const { children, fullWidth = false } = props;
+  return (
+    <Container fullWidth={fullWidth} {...props}>
+      {children}
+    </Container>
+  );
 }
 
-const Container = styled.button<Props>``;
+const Container = styled.button<Props>`
+  ${({ theme }) => theme.typo.body1Bold}
+  color: ${({ theme }) => theme.palette.grayScale.g100};
+
+  padding: ${rem(16)} ${rem(24)};
+  border-radius: ${rem(16)};
+  width: ${({ fullWidth }) => (fullWidth ? '100%' : 'auto')};
+  background-color: ${({ theme }) => theme.palette.main.green50};
+
+  &:active {
+    background-color: ${({ theme }) => theme.palette.main.green100};
+  }
+
+  &:disabled {
+    background-color: ${({ theme }) => theme.palette.grayScale.g40};
+    color: ${({ theme }) => theme.palette.grayScale.g70};
+  }
+`;
 
 export { Button };

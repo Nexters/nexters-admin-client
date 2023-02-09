d223189d8af17cf590ec8a5112645604ea5423ba
Minsu Kim <alstn2468_@naver.com>
2023-02-09 13:01:07 +0900

    feat: 어드민 단건, 복수 회원 생성 mock api 추가

--- packages/msw/lib/members/handlers.ts
+++ packages/msw/lib/members/handlers.ts
@@ -29,7 +29,7 @@ const password = rest.put(
 // 사용자 내 정보 조회 mock API
 const me = rest.get(
   process.env.NEXT_PUBLIC_API_URL + API_URL.ME,
-  async (request, response, context) => {
+  (request, response, context) => {
     // 인증 관련 검증 따로 진행하지 않음
     const token = request.headers.get('Authorization');
 
@@ -49,11 +49,14 @@ const me = rest.get(
 // 어드민 복수 회원 조회 mock API
 const members = rest.get(
   process.env.NEXT_PUBLIC_API_URL + API_URL.MEMBERS,
-  async (request, response, context) => {
+  (request, response, context) => {
     // 인증 관련 검증 따로 진행하지 않음
     const token = request.headers.get('Authorization');
 
-    console.log(`%c[GET:${API_URL.ME}]: { token: ${token} }`, 'color: orange');
+    console.log(
+      `%c[GET:${API_URL.MEMBERS}]: { token: ${token} }`,
+      'color: orange',
+    );
 
     return response(
       context.status(200),
@@ -64,6 +67,78 @@ const members = rest.get(
   },
 );
 
-const handlers = [password, me, members] as const;
+// 어드민 단건 회원 생성 mock API
+const createMember = rest.post(
+  (process.env.NEXT_PUBLIC_API_URL = API_URL.MEMBERS),
+  async (request, response, context) => {
+    const {
+      name,
+      gender,
+      email,
+      phoneNumber,
+      generations,
+      position,
+      subPosition,
+      status,
+      isManager,
+    } = await request.json();
+
+    // 인증 관련 검증 따로 진행하지 않음
+    const token = request.headers.get('Authorization');
+    const member = {
+      name,
+      gender,
+      email,
+      phoneNumber,
+      generations,
+      position,
+      subPosition,
+      status,
+      isManager,
+    };
+
+    console.log(
+      `%c[POST:${API_URL.MEMBERS}]: { token: ${token} }`,
+      'color: orange',
+    );
+    console.log(
+      `%c[POST:${API_URL.MEMBERS}]: { member: ${member} }`,
+      'color: orange',
+    );
+
+    return response(context.status(201));
+  },
+);
+
+// 어드민 복수 회원 생성 mock API
+const createMembersBulk = rest.post(
+  (process.env.NEXT_PUBLIC_API_URL = API_URL.MEMBERS_BULK),
+  async (request, response, context) => {
+    // TODO: data 형식 (csv 포맷)으로 보낼지 확인하기
+    const { data } = await request.json();
+
+    // 인증 관련 검증 따로 진행하지 않음
+    const token = request.headers.get('Authorization');
+
+    console.log(
+      `%c[POST:${API_URL.MEMBERS_BULK}]: { token: ${token} }`,
+      'color: orange',
+    );
+    console.log(
+      `%c[POST:${API_URL.MEMBERS_BULK}]: { data: ${data} }`,
+      'color: orange',
+    );
+
+    return response(context.status(201));
+  },
+);
+
+const handlers = [
+  password,
+  me,
+  members,
+  createMember,
+  createMembersBulk,
+] as const;
 
 export { handlers };
--- packages/msw/lib/members/url.ts
+++ packages/msw/lib/members/url.ts
@@ -2,6 +2,7 @@ const API_URL = {
   PASSWORD: '/api/members/password',
   ME: '/api/members/me',
   MEMBERS: 'api/members',
+  MEMBERS_BULK: 'api/members/bulk',
 };
 
 export { API_URL };

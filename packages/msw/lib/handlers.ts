7ef670e761093b0fb42bd5ab36bf4bc169d9a792
Minsu Kim <alstn2468_@naver.com>
2023-02-09 15:15:49 +0900

    feat: 사용자 앱 세션 정보 mock api 추가

--- packages/msw/lib/handlers.ts
+++ packages/msw/lib/handlers.ts
@@ -1,6 +1,11 @@
 import * as auth from './auth';
 import * as members from './members';
+import * as sessions from './sessions';
 
-const handlers = [...auth.handlers, ...members.handlers] as const;
+const handlers = [
+  ...auth.handlers,
+  ...members.handlers,
+  ...sessions.handlers,
+] as const;
 
 export { handlers };

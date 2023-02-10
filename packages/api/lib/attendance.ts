ba55266cb948ab0bbc398b1b21da11f5a2863568
Minsu Kim <alstn2468_@naver.com>
2023-02-10 14:28:25 +0900

    feat: attendance 관련 쿼리, 뮤테이션 훅 추가

--- packages/api/lib/attendance.ts
+++ packages/api/lib/attendance.ts
@@ -1,4 +1,4 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 import { API_URL } from '@weekly/msw';
 
 import { useAxios } from './AxiosProvider';
@@ -18,4 +18,38 @@ function useMemberAttendanceMutation() {
   return useMutation({ mutationFn });
 }
 
-export { useMemberAttendanceMutation };
+// TODO: 반환 데이터 타입 처리
+function useQRCodeQuey() {
+  const axios = useAxios();
+  const queryFn = async () => {
+    const { data } = await axios.get(API_URL.QR_CODE);
+    return data;
+  };
+  return useQuery({ queryKey: ['attendance', 'qr'], queryFn });
+}
+
+// TODO: 반환 데이터 타입 처리
+function useCreateQRCodeMutation() {
+  const axios = useAxios();
+  const mutationFn = async () => {
+    const { data } = await axios.post(API_URL.QR_CODE);
+    return data;
+  };
+  return useMutation({ mutationFn });
+}
+
+function useDeleteQRCodeMutation() {
+  const axios = useAxios();
+  const mutationFn = async () => {
+    const { data } = await axios.delete(API_URL.QR_CODE);
+    return data;
+  };
+  return useMutation({ mutationFn });
+}
+
+export {
+  useCreateQRCodeMutation,
+  useDeleteQRCodeMutation,
+  useMemberAttendanceMutation,
+  useQRCodeQuey,
+};

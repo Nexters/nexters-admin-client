5a44d70fcb75f206d2f997290090b56fb7964b1b
Minsu Kim <alstn2468_@naver.com>
2023-02-13 19:12:04 +0900

    feat: 비밀번호 변경 페이지 title 추가

--- apps/attendance/pages/authentication/password.tsx
+++ apps/attendance/pages/authentication/password.tsx
@@ -1,7 +1,15 @@
+import { NextSeo } from 'next-seo';
+import { Fragment } from 'react';
+
 import { PasswordPage } from '~/components';
 
 function Password() {
-  return <PasswordPage />;
+  return (
+    <Fragment>
+      <NextSeo title='비밀번호 재설정' />
+      <PasswordPage />
+    </Fragment>
+  );
 }
 
 export default Password;

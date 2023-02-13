c0ee4cab87c4e686a9ee6344ca6c3f8399bb2059
Minsu Kim <alstn2468_@naver.com>
2023-02-13 19:28:32 +0900

    chore: utils 내부 함수 주석 추가

--- packages/utils/react/useValidateState.ts
+++ packages/utils/react/useValidateState.ts
@@ -2,6 +2,13 @@ import { useState } from 'react';
 
 type Predicate<T> = (v: T) => boolean;
 
+/**
+ * 검증이 필요한 상태를 사용할 때 사용하는 커스텀 훅
+ *
+ * @param initalValue - 상태의 초기값
+ * @param validationFunctions - 상태 검증함수 배열
+ * @returns 검증 상태 객체
+ */
 function useValidateState<T>(
   initalValue: T,
   validationFunctions: Array<Predicate<T>>,

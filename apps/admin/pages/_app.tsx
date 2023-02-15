e5e0686d44e4a75e4cdc15e911a5b9b4d49c6e87
Minsu Kim <alstn2468_@naver.com>
2023-02-15 09:54:13 +0900

    feat: @next/font 이용해서 로컬 폰트 가져오도록 설정 (#60)

--- apps/admin/pages/_app.tsx
+++ apps/admin/pages/_app.tsx
@@ -1,3 +1,4 @@
+import localFont from '@next/font/local';
 import { QueryClientProvider } from '@weekly/api';
 import { ThemeProvider } from '@weekly/ui/theme';
 import type { NextPage } from 'next';
@@ -8,6 +9,32 @@ type EnhancedAppProps = AppProps & {
   Component: NextPage;
   pageProps: Record<string, unknown>;
 };
+
+const pretandard = localFont({
+  src: [
+    {
+      path: '../public/Pretendard-Bold.subset.woff2',
+      weight: '700',
+      style: 'bold',
+    },
+    {
+      path: '../public/Pretendard-Semibold.subset.woff2',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../public/Pretendard-Medium.subset.woff2',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../public/Pretendard-Regular.subset.woff2',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-pretendard',
+});
 
 function App(props: EnhancedAppProps) {
   const { Component, pageProps } = props;
@@ -31,7 +58,16 @@ function App(props: EnhancedAppProps) {
   }
   return (
     <QueryClientProvider>
-      <ThemeProvider>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
+      <ThemeProvider>
+        <style global jsx>
+          {`
+            html {
+              font-family: ${pretandard.style.fontFamily};
+            }
+          `}
+        </style>
+        {getLayout(<Component {...pageProps} />)}
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }

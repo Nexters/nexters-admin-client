2ea8b47ad05ad408766eef8d0dc890af88052257
Minsu Kim <alstn2468_@naver.com>
2023-02-16 22:51:57 +0900

    feat: 카메라 버튼 기능, 소셜 링크 연동 추가

--- apps/attendance/components/HomePage/index.tsx
+++ apps/attendance/components/HomePage/index.tsx
@@ -1,10 +1,16 @@
 import { Icon, styled } from '@weekly/ui';
-import { Fragment } from 'react';
 
+import { CameraButton } from './CameraButton';
 import { SessionCard } from './SessionCard';
+import { SocialLinks } from './SocialLinks';
 
 function HomePage() {
-  const isEmpty = true;
+  const data = {
+    type: 'session',
+    date: new Date(),
+    week: 1,
+    description: 'OT & 팀빌딩',
+  };
   return (
     <Container>
       <MenuButton>
@@ -12,36 +18,16 @@ function HomePage() {
       </MenuButton>
       <SessionCard
         type='session'
-        date={new Date()}
-        week={1}
-        description='OT & 팀빌딩'
+        date={data.date}
+        week={data.week}
+        description={data.description}
       />
       <Description>
-        {isEmpty
+        {data.type === 'empty'
           ? '넥스터즈의 정보를 빠르게 받아보세요 :)'
           : '스크린의 QR코드를 찍으면 출석체크 할 수 있어요.'}
       </Description>
-      {isEmpty ? (
-        <SubButtonContainer>
-          <Fragment>
-            <SubButton>
-              <Icon name='link' />
-            </SubButton>
-            <SubButton>
-              <Icon name='facebook' />
-            </SubButton>
-            <SubButton>
-              <Icon name='instagram' />
-            </SubButton>
-          </Fragment>
-        </SubButtonContainer>
-      ) : (
-        <CameraSubButtonContainer>
-          <SubButton>
-            <Icon name='camera' />
-          </SubButton>
-        </CameraSubButtonContainer>
-      )}
+      {data.type === 'empty' ? <SocialLinks /> : <CameraButton />}
     </Container>
   );
 }
@@ -68,28 +54,6 @@ const Description = styled.p`
   ${({ theme }) => theme.typo.body2Medium}
   margin-top: ${({ theme }) => theme.rem(20)};
   color: ${({ theme }) => theme.palette.grayScale.white};
-`;
-
-const SubButtonContainer = styled.div`
-  margin-top: ${({ theme }) => theme.rem(12)};
-`;
-
-const CameraSubButtonContainer = styled.div`
-  position: absolute;
-  left: 50%;
-  transform: translateX(-50%);
-  bottom: ${({ theme }) => theme.rem(32)};
-`;
-
-const SubButton = styled.button`
-  display: inline-flex;
-  justify-content: center;
-  align-items: center;
-  width: ${({ theme }) => theme.rem(40)};
-  height: ${({ theme }) => theme.rem(40)};
-  margin: ${({ theme }) => `0 ${theme.rem(8)}`};
-  border-radius: ${({ theme }) => theme.rem(8)};
-  background-color: ${({ theme }) => theme.palette.grayScale.g80};
 `;
 
 export { HomePage };

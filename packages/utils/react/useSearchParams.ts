dfe9db509d229c8ffeb9d17f5dea79173dfb429f
Minsu Kim <alstn2468_@naver.com>
2023-02-17 15:18:46 +0900

    refac: useSearchParams 살짝 수정

--- packages/utils/react/useSearchParams.ts
+++ packages/utils/react/useSearchParams.ts
@@ -3,26 +3,22 @@ import { useRouter } from 'next/router';
 function useSearchParams() {
   const { pathname, replace, push, query } = useRouter();
 
-  const get = (key?: string) => {
-    return key ? query[key] : query;
-  };
+  const reset = () => replace(pathname);
+  const get = (key?: string) => (key ? query[key] : query);
 
   /**
    * @param args  {key, value} 형태의 배열을 인자로 넘겨 한번에 여러 파라미터 세팅
    */
   const set = (
     args: { key: string; value: string }[],
-    option?: { replace: boolean },
+    option: { replace: boolean } = { replace: false },
   ) => {
     const base = `${pathname}?`;
     const params = args
       .map((arg) => `${arg.key}=${encodeURIComponent(arg.value)}`)
       .join('&');
-    option?.replace ? replace(`${base}${params}`) : push(`${base}${params}`);
-  };
-
-  const reset = () => {
-    replace(`${pathname}`);
+    const url = base + params;
+    option.replace ? replace(url) : push(url);
   };
 
   return { get, set, reset };

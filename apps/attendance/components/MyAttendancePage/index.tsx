cf99a88c10732157dd4d3d4dbacad4f67c6dba70
Minsu Kim <alstn2468_@naver.com>
2023-02-19 17:04:53 +0900

    feat: 출석 정보 없는 경우 컴포넌트 추가

--- apps/attendance/components/MyAttendancePage/index.tsx
+++ apps/attendance/components/MyAttendancePage/index.tsx
@@ -1,9 +1,49 @@
+import { styled } from '@weekly/ui';
 import { Fragment } from 'react';
 
+import { Empty } from './Empty';
+
 function MyAttendancePage() {
-  return <Container />;
+  const isEmpty = true;
+  return (
+    <Container>
+      <HeaderContainer>
+        <Title>내 출석 정보</Title>
+        {!isEmpty && (
+          <Description>
+            22기 내 출석 정보는 03.11까지 확인 가능해요.
+          </Description>
+        )}
+      </HeaderContainer>
+      <AttendanceContainer>
+        <Empty />
+      </AttendanceContainer>
+    </Container>
+  );
 }
 
 const Container = Fragment;
+
+const HeaderContainer = styled.header`
+  text-align: center;
+`;
+
+const Title = styled.h3`
+  ${({ theme }) => theme.typo.h3Bold};
+  color: ${({ theme }) => theme.palette.grayScale.white};
+  margin-top: ${({ theme }) => theme.rem(36)};
+`;
+
+const Description = styled.p`
+  ${({ theme }) => theme.typo.body2Medium};
+  color: ${({ theme }) => theme.palette.grayScale.g50};
+  margin-top: ${({ theme }) => theme.rem(22)};
+`;
+
+const AttendanceContainer = styled.div`
+  display: flex;
+  height: 100vh;
+  margin-top: ${({ theme }) => theme.rem(22)};
+`;
 
 export { MyAttendancePage };

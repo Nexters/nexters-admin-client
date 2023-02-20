18b3fcbae2f6f1eec6c9046efd5f7a65ebee4ae3
Minsu Kim <alstn2468_@naver.com>
2023-02-21 00:33:38 +0900

    fix: date, datetime format 함수 수정 (#73)

--- packages/utils/common/date.ts
+++ packages/utils/common/date.ts
@@ -2,15 +2,12 @@ function addZero(value: number) {
   return `00${value}`.slice(-2);
 }
 
-function formatYYMMDD(datetime: string) {
-  return new Date(parseInt(datetime))
-    .toISOString()
-    .slice(0, 10)
-    .replace(/-/g, '.');
+function formatYYMMDD(date: string) {
+  return new Date(date).toISOString().slice(0, 10).replace(/-/g, '.');
 }
 
 function formatHHMMSS(datetime: string) {
-  const date = new Date(parseInt(datetime));
+  const date = new Date(datetime);
   return [date.getHours(), date.getMinutes(), date.getSeconds()]
     .map(addZero)
     .join(':');

b7d33e77e627fa4a2d5d8ca20bd66f0cf2668b56
zi-gae <doscmdev@gmail.com>
2023-02-21 03:38:07 +0900

    feat: axios 추상화 & eslint ts 룰 추가

---
+++ packages/api/lib/HttpClient.ts
@@ -0,0 +1,160 @@
+import type {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosResponse,
+  HeadersDefaults,
+  ResponseType,
+} from 'axios';
+import axios from 'axios';
+
+export type QueryParamsType = Record<string | number, any>;
+
+export interface FullRequestParams
+  extends Omit<AxiosRequestConfig, 'data' | 'params' | 'url' | 'responseType'> {
+  /** secure를 `true`로 설정하면 `secureWorker`를 호출합니다. */
+  secure?: boolean;
+  /** request path */
+  path: string;
+  type?: ContentType;
+  query?: QueryParamsType;
+  format?: ResponseType;
+  body?: unknown;
+}
+
+export type RequestParams = Omit<
+  FullRequestParams,
+  'body' | 'method' | 'query' | 'path'
+>;
+
+export interface ApiConfig<SecureData = unknown>
+  extends Omit<AxiosRequestConfig, 'data' | 'cancelToken'> {
+  secureWorker?: (
+    secureData: SecureData | null
+  ) => Promise<AxiosRequestConfig | void> | AxiosRequestConfig | void;
+  secure?: boolean;
+  format?: ResponseType;
+}
+
+// ContentType to Object Style
+export const ContentTypeMap = {
+  Json: 'application/json',
+  FormData: 'multipart/form-data',
+};
+
+export type ContentType = keyof typeof ContentTypeMap;
+
+export class HttpClient<SecureData = unknown> {
+  public instance: AxiosInstance;
+  private secureData: SecureData | null = null;
+  private secureWorker?: ApiConfig<SecureData>['secureWorker'];
+  private secure?: boolean;
+  private format?: ResponseType;
+
+  constructor({
+    secureWorker,
+    secure,
+    format,
+    ...axiosConfig
+  }: ApiConfig<SecureData> = {}) {
+    this.instance = axios.create({
+      ...axiosConfig,
+      baseURL: axiosConfig.baseURL || 'http://www.chulchul.site',
+    });
+    this.secure = secure;
+    this.format = format;
+    this.secureWorker = secureWorker;
+  }
+
+  public setSecureData = (data: SecureData | null) => {
+    this.secureData = data;
+  };
+
+  protected mergeRequestParams(
+    params1: AxiosRequestConfig,
+    params2?: AxiosRequestConfig,
+  ): AxiosRequestConfig {
+    const method = params1.method || (params2 && params2.method);
+
+    return {
+      ...this.instance.defaults,
+      ...params1,
+      ...(params2 || {}),
+      headers: {
+        ...((method &&
+          this.instance.defaults.headers[
+            method.toLowerCase() as keyof HeadersDefaults
+          ]) ||
+          {}),
+        ...(params1.headers || {}),
+        ...((params2 && params2.headers) || {}),
+      },
+    };
+  }
+
+  protected stringifyFormItem(formItem: unknown) {
+    if (typeof formItem === 'object' && formItem !== null) {
+      return JSON.stringify(formItem);
+    } else {
+      return `${formItem}`;
+    }
+  }
+
+  protected createFormData(input: Record<string, unknown>): FormData {
+    return Object.keys(input || {}).reduce((formData, key) => {
+      const property = input[key];
+      const propertyContent: any[] =
+        property instanceof Array ? property : [property];
+
+      for (const formItem of propertyContent) {
+        const isFileType = formItem instanceof Blob || formItem instanceof File;
+        formData.append(
+          key,
+          isFileType ? formItem : this.stringifyFormItem(formItem),
+        );
+      }
+
+      return formData;
+    }, new FormData());
+  }
+
+  public request = async <T = any>({
+    secure,
+    path,
+    type,
+    query,
+    format,
+    body,
+    ...params
+  }: FullRequestParams): Promise<AxiosResponse<T>> => {
+    const secureParams =
+      ((typeof secure === 'boolean' ? secure : this.secure) &&
+        this.secureWorker &&
+        (await this.secureWorker(this.secureData))) ||
+      {};
+    const requestParams = this.mergeRequestParams(params, secureParams);
+    const responseFormat = format || this.format || undefined;
+
+    if (
+      type === ContentTypeMap.FormData &&
+      body &&
+      body !== null &&
+      typeof body === 'object'
+    ) {
+      body = this.createFormData(body as Record<string, unknown>);
+    }
+
+    return this.instance.request({
+      ...requestParams,
+      headers: {
+        ...(requestParams.headers || {}),
+        ...(type && type !== ContentTypeMap.FormData
+          ? { 'Content-Type': type }
+          : {}),
+      },
+      params: query,
+      responseType: responseFormat,
+      data: body,
+      url: path,
+    });
+  };
+}

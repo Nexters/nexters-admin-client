ae364de4b7be101d01437b48f0e21bb8b02f087a
Minsu Kim <alstn2468_@naver.com>
2023-02-26 18:49:44 +0900

    feat: 내출석 정보 페이지 데이터 연결

--- apps/attendance/components/MyAttendancePage/index.tsx
+++ apps/attendance/components/MyAttendancePage/index.tsx
@@ -1,38 +1,53 @@
-import { useMeAttendanceQuery } from '@weekly/api';
+import { useMeAttendanceQuery, useMeQuery } from '@weekly/api';
 import { styled } from '@weekly/ui';
+import { formatMonthDate, useMinimumWaiting } from '@weekly/utils';
 import { Fragment } from 'react';
 
+import { Loader } from '~/components/Loader';
 import { onInvalidTokenError } from '~/utils/error';
 
 import { AttendanceList } from './AttendanceList';
 import { CompletableInfo } from './CompletableInfo';
 import { Empty } from './Empty';
 
+const FULL_SESSION_COUNT = 8 as const;
+
 function MyAttendancePage() {
-  const { data } = useMeAttendanceQuery({
+  const meAttendanceQueryResult = useMeAttendanceQuery({
     onError: onInvalidTokenError,
   });
-  const isCompletable = data?.attendanceData.isCompletable ?? true;
-  const score = data?.attendanceData.score ?? 0;
-  const attendances = data?.attendanceData.attendances ?? [];
-  const isFull = attendances.length === 8;
+  const meQueryResult = useMeQuery({ onError: onInvalidTokenError });
+  const wait = useMinimumWaiting();
+  const generation = meQueryResult.data?.generation;
+  const isLoading = meAttendanceQueryResult.isLoading || meQueryResult.isLoading || wait;
+  const isCompletable = meAttendanceQueryResult.data?.attendanceData.isCompletable ?? true;
+  const score = meAttendanceQueryResult.data?.attendanceData.score ?? 0;
+  const attendances = meAttendanceQueryResult.data?.attendanceData.attendances ?? [];
+  const isFull = attendances.length === FULL_SESSION_COUNT;
   const isEmpty = attendances.length === 0;
+  const lastSessionDate = attendances[FULL_SESSION_COUNT - 1]?.sessionDate;
   return (
     <Container>
-      <HeaderContainer isFull={isFull}>
-        <Title>내 출석 정보</Title>
-        {isFull && (
-          <Description>
-            22기 내 출석 정보는 03.11까지 확인 가능해요.
-          </Description>
+      {isLoading
+        ? <Loader />
+        : (
+          <Fragment>
+            <HeaderContainer isFull={isFull}>
+              <Title>내 출석 정보</Title>
+              {isFull && generation && lastSessionDate && (
+                <Description>
+                  {`${generation}기`} 내 출석 정보는 {formatMonthDate(lastSessionDate)}까지 확인 가능해요.
+                </Description>
+              )}
+            </HeaderContainer>
+            {!isEmpty && (
+              <CompletableInfo score={score} isCompletable={isCompletable} />
+            )}
+            <AttendanceContainer>
+              {isEmpty ? <Empty /> : <AttendanceList attendances={attendances} />}
+            </AttendanceContainer>
+          </Fragment>
         )}
-      </HeaderContainer>
-      {!isEmpty && (
-        <CompletableInfo score={score} isCompletable={isCompletable} />
-      )}
-      <AttendanceContainer>
-        {isEmpty ? <Empty /> : <AttendanceList attendances={attendances} />}
-      </AttendanceContainer>
     </Container>
   );
 }

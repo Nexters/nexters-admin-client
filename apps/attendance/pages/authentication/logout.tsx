bb601044dd3acefcb1c4f320b201fa5e48353e24
Minsu Kim <alstn2468_@naver.com>
2023-02-26 17:25:15 +0900

    feat: 로그아웃 시 모든 쿼리 인벨리데이션

--- apps/attendance/pages/authentication/logout.tsx
+++ apps/attendance/pages/authentication/logout.tsx
@@ -1,3 +1,4 @@
+import { queryClient } from '@weekly/api';
 import { useAuthToken, useRedirectEffect } from '@weekly/utils';
 import { useEffect } from 'react';
 
@@ -5,6 +6,7 @@ function Logout() {
   const { removeToken } = useAuthToken();
   useEffect(() => {
     removeToken();
+    queryClient.invalidateQueries();
   }, []);
   useRedirectEffect('/authentication/login');
   return null;

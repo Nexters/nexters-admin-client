e9d00d157837da1359f488e004c67a374afbe79a
Minsu Kim <alstn2468_@naver.com>
2023-02-27 00:43:50 +0900

    feat: 내 출석 정보, 비밀번호 변경 페이지 백버튼 추가 (#85)
    * feat: 백버튼 아이콘 추가
    
    * feat: 내출석 정보 페이지 데이터 연결
    
    * feat: 백버튼 추가
    
    * feat: 메인에서 왔을 경우에 비밀번호 변경 백버튼 보이게 설정
    
    * fix: shouldRender 조건 수정

--- packages/api/lib/common/types.ts
+++ packages/api/lib/common/types.ts
@@ -3,6 +3,7 @@ import type { QueryObserverOptions } from '@tanstack/react-query';
 type OnSuccess<T> = QueryObserverOptions<T>['onSuccess'];
 type OnError = QueryObserverOptions['onError'];
 type QueryParams<T> = {
+  enabled?: QueryObserverOptions['enabled'];
   onSuccess?: OnSuccess<T>;
   onError?: OnError;
 };

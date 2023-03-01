594a00236c1e337bdcd024d6db15374a29c6451c
한규진 <55226431+9yujin@users.noreply.github.com>
2023-03-01 21:36:38 +0900

    feat : 모달 컴포넌트 (#89)
    * feat : 모달 컴포넌트
    
    * chore : 리뷰 반영

--- packages/ui/components/index.ts
+++ packages/ui/components/index.ts
@@ -1,6 +1,7 @@
 export * from './Button';
 export * from './Chip';
 export * from './Dropdown';
+export * from './Modal';
 export * from './Search';
 export * from './Snackbar';
 export * from './TextField';

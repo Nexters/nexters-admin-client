f066c94517769eda4c5965fd19bfcc66c8161a4c
Minsu Kim <alstn2468_@naver.com>
2023-03-04 13:59:34 +0900

    feat: QR 코드 앱 마크업 추가

--- apps/qr/components/GenerateQrcode.tsx
+++ apps/qr/components/GenerateQrcode.tsx
@@ -2,13 +2,9 @@ import QRCodeStyling from 'qr-code-styling';
 import { useEffect, useRef } from 'react';
 
 const qrCode = new QRCodeStyling({
-  width: 300,
-  height: 300,
+  width: 320,
+  height: 320,
   image: '/logo.svg',
-  dotsOptions: {
-    color: '#AFF856',
-    type: 'rounded',
-  },
   imageOptions: {
     crossOrigin: 'anonymous',
   },
@@ -22,18 +18,14 @@ export default function GenerateQrcode(props: Props) {
   const ref = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (ref.current) qrCode.append(ref.current);
+    if (ref.current) {
+      qrCode.append(ref.current);
+    }
   }, []);
 
   useEffect(() => {
-    qrCode.update({
-      data: props.callbackUrl,
-    });
+    qrCode.update({ data: props.callbackUrl });
   }, []);
 
-  return (
-    <div className='App'>
-      <div ref={ref} />
-    </div>
-  );
+  return <div ref={ref} />;
 }
